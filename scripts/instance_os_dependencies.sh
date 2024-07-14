67b99f08f6197f4f6acb2f29b8eb4601b04cc3dd
Antonio Ricardo de Araujo <aricardo.araujo@gmail.com>
2024-07-14 16:28:30 -0300

    Correção dos arquivos de configuração

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# Atualiza a lista de pacotes disponíveis
 sudo apt update
-# Install Python3 pip
+
+# Instala Python3 e pip
 sudo apt install -y python3-pip
 
-# Install Nginx
+# Instala Nginx
 sudo apt install -y nginx
 
-# Install Virtualenv
+# Instala Virtualenv
 sudo apt install -y virtualenv
+
+# Instala pkg-config (necessário para compilar certos pacotes)
+sudo apt install -y pkg-config
+
+# Instala dependências para compilar mysqlclient
+sudo apt install -y python3-dev default-libmysqlclient-dev build-essential
