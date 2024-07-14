8900ba17f12284030b9dcdd8fa9259f136f69554
Antonio Ricardo de Araújo <aaraujo@SUNTECH.INT>
2024-07-14 11:46:28 -0300

    Correção do arquivo nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 PROJECT_MAIN_DIR_NAME="django-whatsappapi"
 
 # Replace {FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS} with the folder name where your nginx configuration file exists
-FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS="core"
+FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS="whatsappapi"
 
 # Reload systemd daemon
 sudo systemctl daemon-reload
